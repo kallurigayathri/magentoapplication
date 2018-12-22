fcfabf56e1a91ab1dec0ff31ceb94f44b9c0b53c
Mounika <Mounika@MOUNIKA>
2018-12-22 16:27:10 +0530

    firstcommit

---
+++ mavenproject/src/main/java/mavenproject/magento.java
@@ -0,0 +1,33 @@
+package mavenproject;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class magento 
+{
+	public static void main(String[] args) throws Exception {
+		ChromeDriver driver=new ChromeDriver();
+		String url="http://www.magento.com";
+		
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get(url);
+		
+      Thread.sleep(3000);
+      
+      driver.findElement(By.linkText("My Account")).click();
+      Thread.sleep(3000);
+      driver.findElement(By.id("email")).sendKeys("nitinmanjunath1991@gmail.com");
+      Thread.sleep(3000);
+      driver.findElement(By.id("pass")).sendKeys("Welcome123");
+      Thread.sleep(3000);
+      driver.findElement(By.id("send2")).click();
+      Thread.sleep(3000);
+      driver.findElement(By.linkText("Log Out")).click();
+      Thread.sleep(3000);
+      
+      driver.close();
+	}
+}
