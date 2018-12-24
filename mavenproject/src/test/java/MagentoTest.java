575e7887973687566c0c39460e51b68fa2989fea
Mounika <Mounika@MOUNIKA>
2018-12-24 11:05:45 +0530

    perfect

--- mavenproject/src/test/java/MagentoTest.java
+++ mavenproject/src/test/java/MagentoTest.java
@@ -4,14 +4,14 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class MagentoTest 
 {
 	@Test
 	public  void credentials() throws Exception {
-		WebDriver driver=new FirefoxDriver();
+		WebDriver driver=new ChromeDriver();
 		String url="http://www.magento.com";
 		
 		driver.manage().window().maximize();
